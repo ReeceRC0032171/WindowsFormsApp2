e402d6f03632d432e82765fa5e68d011602eaedf
RC0032171 <RC0032171@CRO-24-01>
2019-04-16 10:45:32 +0100

    k

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -21,13 +21,26 @@ namespace WindowsFormsApp2
         {
             try
             {
-                double resistor1 = Double.Parse(textBox1.Text);
-                double resistor2 = Double.Parse(textBox2.Text);
-                double resistor3 = Double.Parse(textBox8.Text);
-                double resistor4 = Double.Parse(textBox10.Text);
+                double resistor1 = double.Parse(textBox1.Text);
+                // reads values from textbox 1 and inputs to resistor 1
+                double resistor2 = double.Parse(textBox2.Text);
+                double resistor3 = 0.0;
+                if (textBox8.Text == "0")
+                    resistor3 = double.PositiveInfinity;
+                else
+                    resistor3 = double.Parse(textBox8.Text);
+                // tests textbox 3 for a zero, and returns infinity if it is
+                double resistor4 = 0.0;
+                if (textBox8.Text == "0")
+                    resistor1 = double.PositiveInfinity;
+                else
+                    resistor3 = double.Parse(textBox10.Text);
+                
 
                 double totalresistance = 1.0 / (resistor1 + 1.0 / resistor2 + 1.0 / resistor3 + 1.0 / resistor4 + 1.0);
+                // calculates the total resistance of the parallel circuit by division
                 MessageBox.Show("total resistance =" + totalresistance.ToString("0.00") + "ohms");
+                // shows message of the total resistance in ohms
             }
             catch
             {
@@ -48,40 +61,16 @@ namespace WindowsFormsApp2
         private void button2_Click(object sender, EventArgs e)
         {
             double resistor1 = double.Parse(textBox7.Text);
+            // reads values from textbox 1 and inputs to resistor 1
             double resistor2 = double.Parse(textBox3.Text);
             double resistor3 = double.Parse(textBox4.Text);
+            // reads values from textbox 3 and inputs to resistor 3
             double resistor4 = double.Parse(textBox5.Text);
 
             double totalresistance = resistor1 + resistor2 + resistor3 + resistor4;
-           
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            // calculates the total resistance of the series circuit
+            MessageBox.Show("total resistance =" + totalresistance.ToString("0.00") + "ohms");
+            // display the total resistance of the series circuit with the unit ohms
 
 
         }
